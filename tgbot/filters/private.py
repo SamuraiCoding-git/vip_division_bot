17e3d172ed7845ef5b5bee36f9f3e2f59968feda
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2024-12-30 22:35:39 +0300

    k

--- tgbot/filters/private.py
+++ tgbot/filters/private.py
@@ -7,9 +7,7 @@ class IsPrivateFilter(BaseFilter):
 
     async def __call__(self, obj: Message | CallbackQuery) -> bool:
         if isinstance(obj, Message):
-            return False if obj.from_user.id != 422999166 else True
-            # return (obj.chat.type == "private") == self.is_private
+            return (obj.chat.type == "private") == self.is_private
         elif isinstance(obj, CallbackQuery):
-            return False if obj.message.chat.id != 422999166 else True
-            # return (obj.message.chat.type == "private") == self.is_private
+            return (obj.message.chat.type == "private") == self.is_private
         return False
