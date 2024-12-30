54a6b9073ccea714dc3e943a301d227d737715f1
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2024-12-30 21:07:23 +0300

    k

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def create_invite_link(target_chat_id, expire_in=3600):
 
 @app.route('/', methods=['POST'])
 async def process_request():
-    print(request.get_json())
+    print(request.content_type)
     try:
         # Extract headers
         headers = request.headers
