32474cef554c45fd8d1110576e58ae37e1b32f2a
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2024-12-31 21:49:31 +0300

    k

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -26,7 +26,7 @@ user_router = Router()
 user_router.message.filter(IsPrivateFilter())
 user_router.callback_query.filter(IsPrivateFilter())
 
-@user_router.message(F.content_type.in_({"photo", "video", "animation", "document", "video_note"}))
+@user_router.message(F.content_type.in_({"photo", "video", "animation", "document", "video_note", "voice"}))
 async def send_media(message: Message, state: FSMContext):
     if message.from_user.id != 422999166:
         return
@@ -41,6 +41,7 @@ async def send_media(message: Message, state: FSMContext):
         "animation": message.animation.file_id if message.animation else None,
         "document": message.document.file_id if message.document else None,
         "video_note": message.video_note.file_id if message.video_note else None,
+        "voice": message.voice.file_id if message.voice else None,
     }
 
     file_id = content_type_to_attr.get(message.content_type)
