60c8b7afd1a27cc0e5edb1d5439eaafc3d4bd33c
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2025-01-15 12:48:10 +0300

    k

--- infrastructure/database/models/plans.py
+++ infrastructure/database/models/plans.py
@@ -1,21 +1,21 @@
-from typing import Optional
-
-from sqlalchemy import BIGINT, ForeignKey, Boolean
+from sqlalchemy import BIGINT, Float, Integer
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base, TimestampMixin, TableNameMixin
 
 
-class User(Base, TimestampMixin, TableNameMixin):
-    id: Mapped[int] = mapped_column(BIGINT, primary_key=True, autoincrement=False)
-    username: Mapped[Optional[str]] = mapped_column(String(128))
-    full_name: Mapped[str] = mapped_column(String(128))
-    plan_id: Mapped[Optional[int]] = mapped_column(BIGINT, ForeignKey("plans.id"), nullable=True)
-    is_recurrent: Mapped[bool] = mapped_column(Boolean, default=False)
+class Plan(Base, TimestampMixin, TableNameMixin):
+    id: Mapped[int] = mapped_column(BIGINT, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(256))
+    original_price: Mapped[float] = mapped_column(Float, nullable=False)
+    discounted_price: Mapped[float] = mapped_column(Float, nullable=False)
+    usd_price: Mapped[float] = mapped_column(Float, nullable=False)
+    duration: Mapped[int] = mapped_column(Integer)
 
-    plan: Mapped["Plan"] = relationship("Plan", back_populates="users")
-    orders: Mapped[list["Order"]] = relationship("Order", back_populates="user")
+    users: Mapped[list["User"]] = relationship("User", back_populates="plan")
 
     def __repr__(self):
-        return f"<User {self.id} {self.username} {self.full_name} Plan: {self.plan_id}>"
+        return (
+            f"<Plan {self.id} {self.name} {self.original_price} {self.discounted_price} {self.duration}>"
+        )
