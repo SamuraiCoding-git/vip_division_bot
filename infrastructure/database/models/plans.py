72fd26f25d65e78ae8b93823e49a68438677fc1f
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2025-01-15 12:39:48 +0300

    k

--- infrastructure/database/models/plans.py
+++ infrastructure/database/models/plans.py
@@ -1,5 +1,5 @@
 from sqlalchemy import BIGINT, DECIMAL, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from infrastructure.database.models import Base
 from infrastructure.database.models.base import TimestampMixin, TableNameMixin
@@ -11,3 +11,5 @@ class Plan(Base, TimestampMixin, TableNameMixin):
     original_price: Mapped[DECIMAL] = mapped_column(DECIMAL(10, 2), nullable=False)
     discounted_price: Mapped[DECIMAL] = mapped_column(DECIMAL(10, 2), nullable=False)
     duration: Mapped[int] = mapped_column(Integer)
+
+    users: Mapped[list["User"]] = relationship("User", back_populates="plan")
