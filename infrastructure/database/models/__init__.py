b9d19c1b2ab06134abd5daed95477c180112fdc6
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2025-01-15 12:43:20 +0300

    k

--- infrastructure/database/models/__init__.py
+++ infrastructure/database/models/__init__.py
@@ -2,10 +2,10 @@ from .base import Base
 from .users import User
 from .plans import Plan
 from .order import Order
-from .subscription import Subscription
-from .payment import Payment
-from .deep_link import DeepLink
-from .log import Log
-from .admin import Admin
-from .setting import Settings
-from .enums import PaymentMethod, SubscriptionStatus, Source
+# from .subscription import Subscription
+# from .payment import Payment
+# from .deep_link import DeepLink
+# from .log import Log
+# from .admin import Admin
+# from .setting import Settings
+# from .enums import PaymentMethod, SubscriptionStatus, Source
