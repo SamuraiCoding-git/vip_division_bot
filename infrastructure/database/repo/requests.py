9fdb28e2ef79c6096dd0ea529a24b4e6de1eaef8
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2025-01-18 20:43:10 +0300

    k

--- infrastructure/database/repo/requests.py
+++ infrastructure/database/repo/requests.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from infrastructure.database.repo.admin import AdminRepo
 from infrastructure.database.repo.order import OrderRepo
 from infrastructure.database.repo.plans import PlanRepo
 from infrastructure.database.repo.users import UserRepo
@@ -19,21 +20,16 @@ class RequestsRepo:
 
     @property
     def users(self) -> UserRepo:
-        """
-        The User repository sessions are required to manage user operations.
-        """
         return UserRepo(self.session)
 
     @property
     def plans(self) -> PlanRepo:
-        """
-        The User repository sessions are required to manage user operations.
-        """
         return PlanRepo(self.session)
 
     @property
     def orders(self) -> OrderRepo:
-        """
-        The User repository sessions are required to manage user operations.
-        """
         return OrderRepo(self.session)
+
+    @property
+    def admins(self) -> AdminRepo:
+        return AdminRepo(self.session)
