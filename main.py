aa5067fcba613dd2235d034d78a4252d7be9ac1d
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2025-01-24 17:12:52 +0300

    k

--- main.py
+++ main.py
@@ -94,6 +94,8 @@ def unban_user_from_chat_or_channel(chat_id, user_id):
             print(f"Failed to unban user {user_id} from chat {chat_id}: {response.text}")
         return response.ok
     except Exception as e:
+        if 'administrator' in str(e):
+            return response.ok
         print(f"Error unbanning user {user_id} from chat {chat_id}: {e}")
         return False
 
