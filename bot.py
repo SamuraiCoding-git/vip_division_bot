7e2d91a0d5bef7293d78686c13ffafc7f012b854
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2025-02-06 16:40:09 +0300

    k

--- bot.py
+++ bot.py
@@ -95,7 +95,7 @@ def setup_logging():
 
 async def start_scheduler(bot: Bot, config: Config):
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(check_subscriptions, "cron", hour=13, minute=40, args=[bot, config])
+    scheduler.add_job(check_subscriptions, "cron", hour=13, minute=41, args=[bot, config])
     scheduler.start()
 
 def get_storage(config):
